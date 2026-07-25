ce9bedf5c1443aefdff64078922a9b20a1b372b3
gamath99 <gamamathurin1@gmail.com>
2026-07-25 17:19:42 -0400

    p3 - reated HUD to display score, max and hi score with persistent file storage

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,7 +8,7 @@ from arsenal import Arsenal
 from alien_fleet import Alienfleet
 from time import sleep
 from button import Button
-
+from hud import HUD
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -18,7 +18,6 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
         self.settings.initialize_dynamic_settings()
-        self.game_stats = GameStats(self)
 
         self.screen = pygame.display.set_mode((self.settings.screen_w, self.settings.screen_h))
         pygame.display.set_caption(self.settings.name)
@@ -26,6 +25,8 @@ class AlienInvasion:
         self.bg = pygame.image.load(self.settings.bg_file)
         self.bg = pygame.transform.scale(self.bg, (self.settings.screen_w, self.settings.screen_h))
 
+        self.game_stats = GameStats(self)
+        self.HUD = HUD(self)
         self.running = True
         self.clock = pygame.time.Clock()
 
@@ -74,6 +75,7 @@ class AlienInvasion:
             self.impact_sound.play()    
             self.impact_sound.fadeout(500)
             self.game_stats.update(collisions)
+            self.HUD.update_scores()
 
         if self.alien_fleet.check_destroyed_status():
             self._reset_level()
@@ -104,10 +106,9 @@ class AlienInvasion:
 
 
     def restart_game(self):
-        self.settings.initialize_dynamic_settings()
-        # reset game stats
+        self.settings.initialize_dynamic_settings()    
         self.game_stats.reset_stats()
-        # update HUD scores
+        self.HUD.update_scores()
         self._reset_level()
         self.ship._center_ship()
         self.game_active = True
@@ -118,7 +119,7 @@ class AlienInvasion:
         self.screen.blit(self.bg, (0, 0))
         self.ship.draw()
         self.alien_fleet.draw()  # Draw all aliens in the fleet
-        # draw HUD
+        self.HUD.draw()
 
         if not self.game_active:
             self.play_button.draw()
@@ -130,6 +131,7 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+                self.game_stats.save_scores()
                 pygame.quit()
                 sys.exit() # Limit the frame rate to 60 FPS
             elif event.type == pygame.KEYDOWN and self.game_active == True:
@@ -165,8 +167,10 @@ class AlienInvasion:
                 # play the laser sound
                 self.laser_sound.play()    
                 self.laser_sound.fadeout(250)  # Fade out the sound after 250 milliseconds
+
         elif event.key == pygame.K_q:
             self.running = False
+            self.game_stats.save_scores()
             pygame.quit()
             sys.exit() # Limit the frame rate to 60 FPS           
             
